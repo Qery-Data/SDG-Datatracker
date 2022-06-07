11baecb37f074a1950a9e2d88b3d4a37e452812f
aloenne-qery <alf.lonne@qery.no>
2022-06-07 18:50:17 +0200

    fix viz

--- SDG_3.py
+++ SDG_3.py
@@ -38,15 +38,23 @@ headers = {
     }
 response = requests.request("POST", url, headers=headers)
 
-#3.1.1 Maternal mortality ratio Nordics (OECD) (VnalL) 
-oecd_url='https://stats.oecd.org/SDMX-JSON/data/HEALTH_STAT/MATIMATM.TXCMMMTX.DNK+FIN+ISL+NOR+SWE/all?startTime=2000'
-result = requests.get(oecd_url, headers={'Accept': 'text/csv'})
-df=pd.read_csv(io.StringIO(result.text))
-df_new = df.pivot(index='Country', columns='Year', values='Value')
-df_new.insert(loc=0, column="Flags", value=[':dk:',':fi:',':is:',':no:',':se:'])
+#3.1.1 Maternal mortality ratio Nordics (VnalL) 
+df_csv = pd.read_csv("https://data.un.org/ws/rest/data/IAEG-SDGs,DF_SDG_GLH,1.8/..SH_STA_MORT.208+246+352+578+752.........../ALL/?detail=full&startPeriod=1967-01-01&dimensionAtObservation=TIME_PERIOD&&lastNObservations=1&format=csv")
+df_new = df_csv.pivot(index='REF_AREA', columns='TIME_PERIOD', values='OBS_VALUE')
+df_new.rename(index={208: 'Denmark', 246: 'Finland', 352: 'Iceland', 578:'Norway',752:'Sweden'},inplace=True)
+data_date = str(df_new.columns[0])
 df_new.to_csv('data/3_1_1_Maternal_Mortality_Ratio_Nordics.csv', index=True)
+title_date = 'Maternal deaths per 100,000 live births. ' ' Data for ' + data_date + '.'
 #Update DW
 chartid = 'VnalL'
+url = "https://api.datawrapper.de/v3/charts/" + chartid + '/'
+payload = {"metadata": {"describe": {"intro": title_date}}}
+headers = {
+    "Authorization": ("Bearer " + access_token),
+    "Accept": "*/*",
+    "Content-Type": "application/json"
+    }
+response = requests.request("PATCH", url, json=payload, headers=headers)
 url = "https://api.datawrapper.de/v3/charts/" + chartid + '/publish/'
 headers = {
     "Authorization": ("Bearer " + access_token),
