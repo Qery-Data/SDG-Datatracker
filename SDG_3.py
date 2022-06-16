39f9e1f59b09c4c4f0ac0e18830ec3199b1530fd
aloenne-qery <alf.lonne@qery.no>
2022-06-16 15:08:24 +0200

    new viz 3_7_2

--- SDG_3.py
+++ SDG_3.py
@@ -664,13 +664,56 @@ headers = {
     }
 response = requests.request("POST", url, headers=headers)
 
-#3.6.1 Women Family Planning SDG regions (V3T88)
+#3.7.1 Women Family Planning SDG regions (V3T88)
 df_csv = pd.read_csv('https://data.un.org/ws/rest/data/IAEG-SDGs,DF_SDG_GLH,1.8/..SH_FPL_MTMM.9+62+513+747+753+202+419............../ALL/?detail=full&dimensionAtObservation=TIME_PERIOD&format=csv')
 df_new = df_csv.pivot(index='TIME_PERIOD', columns='REF_AREA', values='OBS_VALUE')
 df_new.rename(columns={9: 'Oceania', 62: 'Central and Southern Asia', 202: 'Sub-Saharan Africa', 419: 'Latin America and the Caribbean', 513: 'Europe and Northern America', 747: 'Northern Africa and Western Asia', 753: 'Eastern and South-Eastern Asia'},inplace=True)
 df_new.to_csv('data/3_7_1_Women_Family_Planning_SDG_Regions.csv', index=True)
 #Update DW
 chartid = 'V3T88'
+url = "https://api.datawrapper.de/v3/charts/" + chartid + '/publish/'
+headers = {
+    "Authorization": ("Bearer " + access_token),
+    "Accept": "*/*"
+    }
+response = requests.request("POST", url, headers=headers)
+
+#3.7.1 Adolescent birth rate World (SLabG)
+df_csv = pd.read_csv('https://data.un.org/ws/rest/data/IAEG-SDGs,DF_SDG_GLH,1.8/..SP_DYN_ADKL.1.........../ALL/?detail=full&dimensionAtObservation=TIME_PERIOD&format=csv')
+df_new = df_csv.pivot(index='REF_AREA', columns='TIME_PERIOD', values='OBS_VALUE')
+df_new.rename(index={1:'World'}, inplace=True)
+df_new.to_csv('data/3_7_2_Adolescent_Birth_Rate_World_Total.csv', index=True)
+#Update DW
+chartid = 'SLabG'
+url = "https://api.datawrapper.de/v3/charts/" + chartid + '/publish/'
+headers = {
+    "Authorization": ("Bearer " + access_token),
+    "Accept": "*/*"
+    }
+response = requests.request("POST", url, headers=headers)
+
+#3.7.2 Adolescent Birth Rate SDG regions (uRSmS)
+df_csv = pd.read_csv('https://data.un.org/ws/rest/data/IAEG-SDGs,DF_SDG_GLH,1.8/..SP_DYN_ADKL.9+62+513+747+753+202+419............../ALL/?detail=full&dimensionAtObservation=TIME_PERIOD&format=csv')
+df_new = df_csv.pivot(index='TIME_PERIOD', columns='REF_AREA', values='OBS_VALUE')
+df_new.rename(columns={9: 'Oceania', 62: 'Central and Southern Asia', 202: 'Sub-Saharan Africa', 419: 'Latin America and the Caribbean', 513: 'Europe and Northern America', 747: 'Northern Africa and Western Asia', 753: 'Eastern and South-Eastern Asia'},inplace=True)
+df_new.to_csv('data/3_7_2_Adolescent_Birth_Rate_SDG_Regions.csv', index=True)
+#Update DW
+chartid = 'uRSmS'
+url = "https://api.datawrapper.de/v3/charts/" + chartid + '/publish/'
+headers = {
+    "Authorization": ("Bearer " + access_token),
+    "Accept": "*/*"
+    }
+response = requests.request("POST", url, headers=headers)
+
+#3.7.2 Adolescent fertility rate OECD (jNvOV)
+oecd_url='https://stats.oecd.org/SDMX-JSON/data/FAMILY/DNK+FIN+ISL+NOR+SWE.TOTAL.FAM20/all?startTime=2000'
+result = requests.get(oecd_url, headers={'Accept': 'text/csv'})
+df=pd.read_csv(io.StringIO(result.text))
+df_new = df.pivot(index='Country', columns='Year', values='Value')
+df_new.to_csv('data/3_7_2_Adolescent_Fertility_Rate_Nordics.csv', index=True)
+#Update DW
+chartid = 'jNvOV'
 url = "https://api.datawrapper.de/v3/charts/" + chartid + '/publish/'
 headers = {
     "Authorization": ("Bearer " + access_token),
