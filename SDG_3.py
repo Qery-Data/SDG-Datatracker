79eff2ca7e8a71029b6b537ec6027cbf8772aa62
aloenne-qery <alf.lonne@qery.no>
2022-06-16 15:50:15 +0200

    3_8_1

--- SDG_3.py
+++ SDG_3.py
@@ -721,3 +721,31 @@ headers = {
     }
 response = requests.request("POST", url, headers=headers)
 
+#3.8.1 Universal Health Care Index World (DUq5f)
+df_csv = pd.read_csv('https://data.un.org/ws/rest/data/IAEG-SDGs,DF_SDG_GLH,1.8/..SH_ACS_UNHC.1.........../ALL/?detail=full&dimensionAtObservation=TIME_PERIOD&format=csv')
+df_new = df_csv.pivot(index='REF_AREA', columns='TIME_PERIOD', values='OBS_VALUE')
+df_new.rename(index={1:'World'}, inplace=True)
+df_new.to_csv('data/3_8_1_Universal_Health_Care_Index_World_Total.csv', index=True)
+#Update DW
+chartid = 'DUq5f'
+url = "https://api.datawrapper.de/v3/charts/" + chartid + '/publish/'
+headers = {
+    "Authorization": ("Bearer " + access_token),
+    "Accept": "*/*"
+    }
+response = requests.request("POST", url, headers=headers)
+
+#3.8.1 Universal Health Care Index SDG regions (DUq5f)
+df_csv = pd.read_csv('https://data.un.org/ws/rest/data/IAEG-SDGs,DF_SDG_GLH,1.8/..SH_ACS_UNHC.9+62+513+747+753+202+419.........../ALL/?detail=full&dimensionAtObservation=TIME_PERIOD&format=csv')
+df_new = df_csv.pivot(index='TIME_PERIOD', columns='REF_AREA', values='OBS_VALUE')
+df_new.rename(columns={9: 'Oceania', 62: 'Central and Southern Asia', 202: 'Sub-Saharan Africa', 419: 'Latin America and the Caribbean', 513: 'Europe and Northern America', 747: 'Northern Africa and Western Asia', 753: 'Eastern and South-Eastern Asia'},inplace=True)
+df_new.to_csv('data/3_8_1_Universal_Health_Care_Index_SDG_Regions.csv', index=True)
+#Update DW
+chartid = 'DUq5f'
+url = "https://api.datawrapper.de/v3/charts/" + chartid + '/publish/'
+headers = {
+    "Authorization": ("Bearer " + access_token),
+    "Accept": "*/*"
+    }
+response = requests.request("POST", url, headers=headers)
+
