b0403e9a579d01c0a1070fe7cb9ec29ae7b71f7c
aloenne-qery <alf.lonne@qery.no>
2022-06-20 20:24:23 +0200

    3_d_2

--- SDG_3.py
+++ SDG_3.py
@@ -1182,11 +1182,39 @@ headers = {
     }
 response = requests.request("POST", url, headers=headers)
 
-#3.d.2 MRSA ECOLI World (xxxxx)
-df_csv = pd.read_csv('https://data.un.org/ws/rest/data/IAEG-SDGs,DF_SDG_GLH,1.8/..SH_BLD_MRSA+SH_BLD_ECOLI.1+9+62+513+747+753+202+419.........../ALL/?detail=full&lastNObservations=1&format=csv')
-df_new = df_csv.pivot(index='REF_AREA', columns='SERIES', values='OBS_VALUE')
+#3.d.2 MRSA World (xxxxx)
+df_csv = pd.read_csv('https://data.un.org/ws/rest/data/IAEG-SDGs,DF_SDG_GLH,1.8/..SH_BLD_MRSA.1+9+62+513+747+753+202+419.........../ALL/?detail=full&dimensionAtObservation=TIME_PERIOD&format=csv')
+df_new = df_csv.pivot(index='REF_AREA', columns='TIME_PERIOD', values='OBS_VALUE')
 df_new.rename(index={1:'World',9: 'Oceania', 62: 'Central and Southern Asia', 202: 'Sub-Saharan Africa', 419: 'Latin America and the Caribbean', 513: 'Europe and Northern America', 747: 'Northern Africa and Western Asia', 753: 'Eastern and South-Eastern Asia'}, inplace=True)
-df_new.to_csv('data/3_d_2_MRSA_ECOLI_World_Total.csv', index=True)
+df_new.to_csv('data/3_d_2_MRSA_World_Total.csv', index=True)
+#Update DW
+chartid = 'xxxxx'
+url = "https://api.datawrapper.de/v3/charts/" + chartid + '/publish/'
+headers = {
+    "Authorization": ("Bearer " + access_token),
+    "Accept": "*/*"
+    }
+response = requests.request("POST", url, headers=headers)
+
+#3.d.2 ECOLI World (xxxxx)
+df_csv = pd.read_csv('https://data.un.org/ws/rest/data/IAEG-SDGs,DF_SDG_GLH,1.8/..SH_BLD_ECOLI.1+9+62+513+747+753+202+419.........../ALL/?detail=full&dimensionAtObservation=TIME_PERIOD&format=csv')
+df_new = df_csv.pivot(index='REF_AREA', columns='TIME_PERIOD', values='OBS_VALUE')
+df_new.rename(index={1:'World',9: 'Oceania', 62: 'Central and Southern Asia', 202: 'Sub-Saharan Africa', 419: 'Latin America and the Caribbean', 513: 'Europe and Northern America', 747: 'Northern Africa and Western Asia', 753: 'Eastern and South-Eastern Asia'}, inplace=True)
+df_new.to_csv('data/3_d_2_ECOLI_World_Total.csv', index=True)
+#Update DW
+chartid = 'xxxxx'
+url = "https://api.datawrapper.de/v3/charts/" + chartid + '/publish/'
+headers = {
+    "Authorization": ("Bearer " + access_token),
+    "Accept": "*/*"
+    }
+response = requests.request("POST", url, headers=headers)
+
+#3.d.2 IHR Capacity Nordics (xxxxx)
+df_csv = pd.read_csv("https://data.un.org/ws/rest/data/IAEG-SDGs,DF_SDG_GLH,1.8/..SH_IHR_CAPS.208+246+352+578+752........SPAR_01+SPAR_02+SPAR_03+SPAR_04+SPAR_05+SPAR_06+SPAR_07+SPAR_08+SPAR_09+SPAR_10+SPAR_11+SPAR_12+SPAR_13.../ALL/?detail=full&lastNObservations=1&format=csv")
+df_new = df_csv.pivot(index='REF_AREA', columns='COMPOSITE_BREAKDOWN', values='OBS_VALUE')
+df_new.rename(index={208: 'Denmark', 246: 'Finland', 352: 'Iceland', 578:'Norway',752:'Sweden'},inplace=True)
+df_new.to_csv('data/3_d_2_IHR_Nordics.csv', index=True)
 #Update DW
 chartid = 'xxxxx'
 url = "https://api.datawrapper.de/v3/charts/" + chartid + '/publish/'
