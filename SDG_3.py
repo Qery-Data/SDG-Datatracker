277bf0bf885eda0f27d33121caa985df190db209
aloenne-qery <alf.lonne@qery.no>
2022-06-20 13:54:32 +0200

    3_d_2

--- SDG_3.py
+++ SDG_3.py
@@ -1181,3 +1181,17 @@ headers = {
     "Accept": "*/*"
     }
 response = requests.request("POST", url, headers=headers)
+
+#3.d.2 MRSA ECOLI World (xxxxx)
+df_csv = pd.read_csv('https://data.un.org/ws/rest/data/IAEG-SDGs,DF_SDG_GLH,1.8/..SH_BLD_MRSA+SH_BLD_ECOLI.1+9+62+513+747+753+202+419.........../ALL/?detail=full&lastNObservations=1&format=csv')
+df_new = df_csv.pivot(index='REF_AREA', columns='SERIES', values='OBS_VALUE')
+df_new.rename(index={1:'World',9: 'Oceania', 62: 'Central and Southern Asia', 202: 'Sub-Saharan Africa', 419: 'Latin America and the Caribbean', 513: 'Europe and Northern America', 747: 'Northern Africa and Western Asia', 753: 'Eastern and South-Eastern Asia'}, inplace=True)
+df_new.to_csv('data/3_d_2_MRSA_ECOLI_World_Total.csv', index=True)
+#Update DW
+chartid = 'xxxxx'
+url = "https://api.datawrapper.de/v3/charts/" + chartid + '/publish/'
+headers = {
+    "Authorization": ("Bearer " + access_token),
+    "Accept": "*/*"
+    }
+response = requests.request("POST", url, headers=headers)
