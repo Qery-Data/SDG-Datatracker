8c76b667e35073e43b230c87e9fd61e36f9c0765
aloenne-qery <alf.lonne@qery.no>
2022-07-17 23:12:51 +0200

    5_2_1

--- SDG_5.py
+++ SDG_5.py
@@ -60,3 +60,15 @@ df_new_area_4.rename(columns={208: 'Denmark', 246: 'Finland', 352: 'Iceland', 57
 df_new_area_4.rename(index={2020: 'Area 4: Marriage and family'}, inplace=True)
 df_all = pd.concat([df_new_area_1,df_new_area_2,df_new_area_3,df_new_area_4], axis=0)
 df_all.to_csv('data/5_1_1_Legal_Frameworks_Nordics.csv', index=True)
+
+#5.2.1 Violence women SDG Regions (L0gR0)
+df_csv = pd.read_csv('https://data.un.org/ws/rest/data/IAEG-SDGs,DF_SDG_GLH,1.8/..VC_VAW_MARR.1+150+15+21+53+30+34+35+143+145+202+419+543..Y_GE15........./ALL/?detail=full&startPeriod=2018-01-01&dimensionAtObservation=TIME_PERIOD&format=csv')
+df_new = df_csv.pivot(index='TIME_PERIOD', columns='REF_AREA', values='OBS_VALUE')
+df_new.rename(columns={1: 'World', 150:'Europe', 15:'Northern Africa', 21:'Northern America', 53:'Australia and New Zeland',30:'Eastern Asia',34:'Southern Asia',35:'South-Eastern Asia', 143:'Central Asia', 145:'Western Asia',202:'Sub-Saharan Africa', 419:'Latin America and the Caribbean', 543:'Oceania (exc. Australia and New Zealand)'},inplace=True)
+df_new.to_csv('data/5_2_1_Violence_Women_SDG_Regions.csv', index=True)
+
+#5.2.1 Violence women Nordics (bKbpd)
+df_csv = pd.read_csv('https://data.un.org/ws/rest/data/IAEG-SDGs,DF_SDG_GLH,1.8/..VC_VAW_MARR.208+246+352+578+752..Y_GE15........./ALL/?detail=full&startPeriod=2018-01-01&dimensionAtObservation=TIME_PERIOD&format=csv')
+df_new = df_csv.pivot(index='TIME_PERIOD', columns='REF_AREA', values='OBS_VALUE')
+df_new.rename(columns={208: 'Denmark', 246: 'Finland', 352: 'Iceland', 578:'Norway',752:'Sweden'},inplace=True)
+df_new.to_csv('data/5_2_1_Violence_Women_Nordics.csv', index=True)
