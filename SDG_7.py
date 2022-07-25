b619012dbf67325407b956bfccfeb49a147b6e1c
aloenne-qery <alf.lonne@qery.no>
2022-07-25 13:25:05 +0200

    7_2

--- SDG_7.py
+++ SDG_7.py
@@ -50,3 +50,28 @@ df_new.rename(columns={53: 'Australia and New Zealand', 62: 'Central and Souther
 df_new = df_new.reindex(columns=['Europe and Northern America','Northern Africa and Western Asia','Sub-Saharan Africa','Central and Southern Asia','Eastern and South-Eastern Asia','Oceania*','Australia and New Zealand','Latin America and the Caribbean'])
 df_new.to_csv('data/7_1_2_Clean_Fuels_Technology_SDG_Regions.csv', index=True)
 
+#7.2.1 Renewable energy share in the total final energy consumption World (JYmiU)
+df_csv = pd.read_csv('https://data.un.org/ws/rest/data/IAEG-SDGs,DF_SDG_GLH,1.9/..EG_FEC_RNEW.1.........../ALL/?detail=full&dimensionAtObservation=TIME_PERIOD&format=csv')
+df_new = df_csv.pivot(index='REF_AREA', columns='TIME_PERIOD', values='OBS_VALUE')
+df_new.rename(index={1:'World'}, inplace=True)
+df_new.to_csv('data/7_2_1_Renewable_Energy_Share_World_Total.csv', index=True)
+
+#7.2.1 Renewable energy share in the total final energy consumption SDG Regions (WYLTV)
+df_csv = pd.read_csv('https://data.un.org/ws/rest/data/IAEG-SDGs,DF_SDG_GLH,1.9/..EG_FEC_RNEW.53+62+513+543+747+753+202+419.........../ALL/?detail=full&startPeriod=2000-01-01&dimensionAtObservation=TIME_PERIOD&format=csv')
+df_new = df_csv.pivot(index='TIME_PERIOD', columns='REF_AREA', values='OBS_VALUE')
+df_new.rename(columns={53: 'Australia and New Zealand', 62: 'Central and Southern Asia', 202: 'Sub-Saharan Africa', 419: 'Latin America and the Caribbean', 513: 'Europe and Northern America', 543: 'Oceania*', 747: 'Northern Africa and Western Asia', 753: 'Eastern and South-Eastern Asia'},inplace=True)
+df_new = df_new.reindex(columns=['Europe and Northern America','Northern Africa and Western Asia','Sub-Saharan Africa','Central and Southern Asia','Eastern and South-Eastern Asia','Oceania*','Australia and New Zealand','Latin America and the Caribbean'])
+df_new.to_csv('data/7_2_1_Renewable_Energy_Share_SDG_Regions.csv', index=True)
+
+#7.2.1 Renewable energy share in the total final energy consumption Nordics (iH0RT)
+df_csv = pd.read_csv('https://data.un.org/ws/rest/data/IAEG-SDGs,DF_SDG_GLH,1.9/..EG_FEC_RNEW.208+246+352+578+752.........../ALL/?detail=full&startPeriod=2000-01-01&dimensionAtObservation=TIME_PERIOD&format=csv')
+df_new = df_csv.pivot(index='TIME_PERIOD', columns='REF_AREA', values='OBS_VALUE')
+df_new.rename(columns={208: 'Denmark', 246: 'Finland', 352: 'Iceland', 578:'Norway',752:'Sweden'},inplace=True)
+df_new.to_csv('data/7_2_1_Renewable_Energy_Share_Nordics.csv', index=True)
+
+#7.2.1 Renewable electricity share in the total electricity generation Nordics (MG14f)
+oecd_url='https://stats.oecd.org/SDMX-JSON/data/GREEN_GROWTH/DNK+FIN+ISL+NOR+SWE+OECD.RE_NRG/all?startTime=2000&endTime=2021'
+result = requests.get(oecd_url, headers={'Accept': 'text/csv'})
+df=pd.read_csv(io.StringIO(result.text))
+df_new = df.pivot(index='Year', columns='Country', values='Value')
+df_new.to_csv('data/7_2_1_Renewable_Electricity_Share_Nordics.csv', index=True)
