f310a35cf68a3405a11cdc772c483963a592e6ea
aloenne-qery <alf.lonne@qery.no>
2022-08-02 11:40:22 +0200

    first

--- SDG_8.py
+++ SDG_8.py
@@ -81,7 +81,7 @@ df_csv = pd.read_csv('https://data.un.org/ws/rest/data/IAEG-SDGs,DF_SDG_GLH,1.9/
 df_new = df_csv.pivot(index='TIME_PERIOD', columns='REF_AREA', values='OBS_VALUE')
 df_new.rename(columns={53: 'Australia and New Zealand', 62: 'Central and Southern Asia', 202: 'Sub-Saharan Africa', 419: 'Latin America and the Caribbean', 513: 'Europe and Northern America', 543: 'Oceania*', 747: 'Northern Africa and Western Asia', 753: 'Eastern and South-Eastern Asia'},inplace=True)
 df_new = df_new.reindex(columns=['Europe and Northern America','Northern Africa and Western Asia','Sub-Saharan Africa','Central and Southern Asia','Eastern and South-Eastern Asia','Oceania*','Australia and New Zealand','Latin America and the Caribbean'])
-df_new.to_csv('data/8_4_2_Domestic_Material_Consumption__GDP_SDG_Regions.csv', index=True)
+df_new.to_csv('data/8_4_2_Domestic_Material_Consumption_GDP_SDG_Regions.csv', index=True)
 
 #8.4.2 Domestic material consumption share SDG Regions (CKrtc)
 df_csv = pd.read_csv('https://data.un.org/ws/rest/data/IAEG-SDGs,DF_SDG_GLH,1.9/..EN_MAT_DOMCMPT.53+62+513+543+747+753+202+419..........._T/?detail=full&startPeriod=2000-01-01&dimensionAtObservation=TIME_PERIOD&format=csv')
