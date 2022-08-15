20067522f5240645a25d9ac18f7d6012e3ca2eaf
aloenne-qery <alf.lonne@qery.no>
2022-08-15 10:30:24 +0200

    10_6

--- SDG_10.py
+++ SDG_10.py
@@ -35,3 +35,10 @@ df_new.rename(columns={208: 'Denmark', 246: 'Finland', 352: 'Iceland', 578:'Norw
 df_new.insert(4,"OECD 2030-target", [1, 2, 12, 2,22, 130,-24], True)
 df_new.rename(index={'FI_FSI_FSANL':'Non-performing loans to total gross loans (%)', 'FI_FSI_FSERA':'Return on assets  (%)', 'FI_FSI_FSKA': 'Regulatory capital to assets (%)', 'FI_FSI_FSKNL':'Non-performing loans net of provisions to capital (%)', 'FI_FSI_FSKRTC': 'Regulatory Tier 1 capital to risk-weighted assets (%)', 'FI_FSI_FSLS': 'Liquid assets to short term liabilities (%)', 'FI_FSI_FSSNO': 'Net open position in foreign exchange to capital (%)'}, inplace=True)
 df_new.to_csv('data/10_5_1_Financial_Soundness_Indicators_Nordics.csv', index=True)
+
+#10.6.1 Membership and Voting rights Developing regions (mAJEt)
+df_csv = pd.read_csv('https://data.un.org/ws/rest/data/IAEG-SDGs,DF_SDG_GLH,1.9/..SG_INT_MBRDEV+SG_INT_VRTDEV.515.........../ALL/?detail=full&lastNObservations=1&format=csv')
+df_new = df_csv.pivot(index='COMPOSITE_BREAKDOWN', columns='SERIES', values='OBS_VALUE')
+df_new.rename(index={'IO_ADB':'Asian Development Bank','IO_AFDB':'African Development Bank', 'IO_ECOSOC':'UN Economic and Social Council', 'IO_FSB': 'Financial Stability Board', 'IO_IADB':'Inter-American Development Bank', 'IO_IBRD':'International Bank for Reconstruction and Development', 'IO_IFC':'International Finance Corporation', 'IO_IMF': 'International Monetary Fund', 'IO_UNGA':'UN General Assembly', 'IO_UNSC':'UN Security Council','IO_WTO':'World Trade Organisation'}, inplace=True)
+df_new.rename(columns={'SG_INT_MBRDEV':'Share of members', 'SG_INT_VRTDEV':'Share of voting rights'},inplace=True)
+df_new.to_csv('data/10_6_1_Membership_Voting_Rights_Developing_Regions.csv', index=True)
