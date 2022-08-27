73fc10a5f57b8c36e8efbfa10a207ccdef922487
aloenne-qery <alf.lonne@qery.no>
2022-08-27 11:19:52 +0200

    12_3

--- SDG_12.py
+++ SDG_12.py
@@ -27,3 +27,8 @@ df_new = df_csv.pivot(index='TIME_PERIOD', columns='REF_AREA', values='OBS_VALUE
 df_new.rename(columns={208: 'Denmark', 246: 'Finland', 352: 'Iceland', 578:'Norway',752:'Sweden'},inplace=True)
 df_new.to_csv('data/12_1_1_SCP_Policies_Nordics.csv', index=True)
 
+#12.3.1 Food loss World and SDG regions (SZ9S3)
+df_csv = pd.read_csv('https://data.un.org/ws/rest/data/IAEG-SDGs,DF_SDG_GLH,1.9/..AG_FLS_PCT.1+53+62+513+543+747+753+202+419.........../ALL/?detail=full&dimensionAtObservation=TIME_PERIOD&format=csv')
+df_new = df_csv.pivot(index='REF_AREA', columns='TIME_PERIOD', values='OBS_VALUE')
+df_new.rename(index={1: 'World', 53: 'Australia and New Zealand', 62: 'Central and Southern Asia', 202: 'Sub-Saharan Africa', 419: 'Latin America and the Caribbean', 513: 'Europe and Northern America',543: 'Oceania*', 747: 'Northern Africa and Western Asia', 753: 'Eastern and South-Eastern Asia'}, inplace=True)
+df_new.to_csv('data/12_3_1_Food_Loss_World_SDG_Regions.csv', index=True)
