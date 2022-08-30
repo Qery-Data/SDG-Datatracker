273d4173a0f745545646e95c9b856b88d56bc8e6
aloenne-qery <alf.lonne@qery.no>
2022-08-30 18:50:48 +0200

    13_1

--- SDG_13.py
+++ SDG_13.py
@@ -9,12 +9,12 @@ import pandas as pd
 os.makedirs('data', exist_ok=True)
 access_token = os.getenv('DW_TOKEN')
 
-#13.1.1 Total greenhouse gas emissions per unit of GDP Nordics (6xFUZ)
-oecd_url='https://stats.oecd.org/SDMX-JSON/data/AIR_GHG/DNK+FIN+ISL+NOR+SWE.GHG.GHG_GDP/all?startTime=1990'
+#13.1.1 Total greenhouse gas emissions Nordics (hAsML)
+oecd_url='https://stats.oecd.org/SDMX-JSON/data/AIR_GHG/DNK+FIN+ISL+NOR+SWE.GHG.TOTAL/all?startTime=1990'
 result = requests.get(oecd_url, headers={'Accept': 'text/csv'})
 df=pd.read_csv(io.StringIO(result.text))
 df_new = df.pivot(index='Year', columns='Country', values='Value')
-df_new.to_csv('data/13_1_1_Total_Greenhouse_Gas_Emissions_Unit_GDP_Nordics.csv', index=True)
+df_new.to_csv('data/13_1_1_Total_Greenhouse_Gas_Emissions_Nordics.csv', index=True)
 
 #13.1.1 Total greenhouse gas emissions per capita Nordics (V6Zeh)
 oecd_url='https://stats.oecd.org/SDMX-JSON/data/AIR_GHG/DNK+FIN+ISL+NOR+SWE.GHG.GHG_CAP/all?startTime=1990'
@@ -22,3 +22,10 @@ result = requests.get(oecd_url, headers={'Accept': 'text/csv'})
 df=pd.read_csv(io.StringIO(result.text))
 df_new = df.pivot(index='Year', columns='Country', values='Value')
 df_new.to_csv('data/13_1_1_Total_Greenhouse_Gas_Emissions_Capita_Nordics.csv', index=True)
+
+#13.1.1 Total greenhouse gas emissions per unit of GDP Nordics (6xFUZ)
+oecd_url='https://stats.oecd.org/SDMX-JSON/data/AIR_GHG/DNK+FIN+ISL+NOR+SWE.GHG.GHG_GDP/all?startTime=1990'
+result = requests.get(oecd_url, headers={'Accept': 'text/csv'})
+df=pd.read_csv(io.StringIO(result.text))
+df_new = df.pivot(index='Year', columns='Country', values='Value')
+df_new.to_csv('data/13_1_1_Total_Greenhouse_Gas_Emissions_Unit_GDP_Nordics.csv', index=True)
