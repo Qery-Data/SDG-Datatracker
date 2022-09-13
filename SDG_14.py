d131d467aa0ed73f9c4e352ed1169250a71c3151
aloenne-qery <alf.lonne@qery.no>
2022-09-13 11:23:59 +0200

    14_1

--- SDG_14.py
+++ SDG_14.py
@@ -9,3 +9,20 @@ import pandas as pd
 os.makedirs('data', exist_ok=True)
 access_token = os.getenv('DW_TOKEN')
 
+#14.1.1 Beach litter per square kilometer World (K3E3U)
+df_csv = pd.read_csv('https://data.un.org/ws/rest/data/IAEG-SDGs,DF_SDG_GLH,1.9/..EN_MAR_BEALITSQ.1.........../ALL/?detail=full&dimensionAtObservation=TIME_PERIOD&format=csv')
+df_new = df_csv.pivot(index='REF_AREA', columns='TIME_PERIOD', values='OBS_VALUE')
+df_new.rename(index={1:'World'}, inplace=True)
+df_new.to_csv('data/14_1_1_Beach_Litter_World_Total.csv', index=True)
+
+#14.1.1 Chlorophyll-a deviations Nordics (Amh1I)
+df_csv = pd.read_csv('https://data.un.org/ws/rest/data/IAEG-SDGs,DF_SDG_GLH,1.9/..EN_MAR_CHLDEV.208+246+352+578+752.........../ALL/?detail=full&dimensionAtObservation=TIME_PERIOD&format=csv')
+df_new = df_csv.pivot(index='TIME_PERIOD', columns='REF_AREA', values='OBS_VALUE')
+df_new.rename(columns={208: 'Denmark', 246: 'Finland', 352: 'Iceland', 578:'Norway',752:'Sweden'},inplace=True)
+df_new.to_csv('data/14_1_1_Chlorophyll_A_Deviations_Nordics.csv', index=True)
+
+#14.1.1 Beach litter per square kilometer  Nordics (CCX2U)
+df_csv = pd.read_csv('https://data.un.org/ws/rest/data/IAEG-SDGs,DF_SDG_GLH,1.9/..EN_MAR_BEALITSQ.208+246+352+578+752.........../ALL/?detail=full&dimensionAtObservation=TIME_PERIOD&format=csv')
+df_new = df_csv.pivot(index='TIME_PERIOD', columns='REF_AREA', values='OBS_VALUE')
+df_new.rename(columns={208: 'Denmark', 246: 'Finland', 352: 'Iceland', 578:'Norway',752:'Sweden'},inplace=True)
+df_new.to_csv('data/14_1_1_Beach_Litter_Nordics.csv', index=True)
