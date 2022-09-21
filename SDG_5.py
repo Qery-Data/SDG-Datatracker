3facea073487cd6929b90dcd9151091648e3c86a
aloenne-qery <alf.lonne@qery.no>
2022-09-21 12:02:58 +0200

    fix to 5_1

--- SDG_5.py
+++ SDG_5.py
@@ -9,7 +9,7 @@ import pandas as pd
 os.makedirs('data', exist_ok=True)
 access_token = os.getenv('DW_TOKEN')
 
-#5.1.1 Legal frameworks World and SDG Regions (zv9jZ)
+#5.1.1 Legal frameworks World and SDG Regions (a0XKn)
 #Area 1 overarching legal frameworks and public life
 df_csv = pd.read_csv('https://data.un.org/ws/rest/data/IAEG-SDGs,DF_SDG_GLH,1.8/..SG_LGL_GENEQLFP.1+53+62+513+747+753+202+419.........../ALL/?detail=full&lastNObservations=1&format=csv')
 df_new = df_csv.pivot(index='TIME_PERIOD', columns='REF_AREA', values='OBS_VALUE')
@@ -35,6 +35,7 @@ df_new.rename(columns={1: 'World', 53: 'Australia and New Zealand', 62: 'Central
 df_new_area_4 = df_new.reindex(columns=['World','Europe and Northern America','Northern Africa and Western Asia','Sub-Saharan Africa','Central and Southern Asia','Eastern and South-Eastern Asia','Australia and New Zealand','Latin America and the Caribbean'])
 df_new_area_4.rename(index={2020: 'Area 4: Marriage and family'}, inplace=True)
 df_all = pd.concat([df_new_area_1,df_new_area_2,df_new_area_3,df_new_area_4], axis=0)
+df_all.loc['Average across all areas'] = df_all.mean(axis=0)
 df_all.to_csv('data/5_1_1_Legal_Frameworks_World_SDG_Regions.csv', index=True)
 
 #5.1.1 Legal frameworks Nordics (IvDU9)
@@ -59,6 +60,7 @@ df_new_area_4 = df_csv.pivot(index='TIME_PERIOD', columns='REF_AREA', values='OB
 df_new_area_4.rename(columns={208: 'Denmark', 246: 'Finland', 352: 'Iceland', 578:'Norway',752:'Sweden'},inplace=True)
 df_new_area_4.rename(index={2020: 'Area 4: Marriage and family'}, inplace=True)
 df_all = pd.concat([df_new_area_1,df_new_area_2,df_new_area_3,df_new_area_4], axis=0)
+df_all.loc['Average across all areas'] = df_all.mean(axis=0)
 df_all.to_csv('data/5_1_1_Legal_Frameworks_Nordics.csv', index=True)
 
 #5.2.1 Violence women World and SDG regions (L0gR0)
