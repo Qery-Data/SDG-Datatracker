9f6668042af9e19ae217c0b6d18f162fed50a4ca
aloenne-qery <alf.lonne@qery.no>
2022-09-29 20:07:12 +0200

    16_2

--- SDG_16.py
+++ SDG_16.py
@@ -44,3 +44,33 @@ df_csv = pd.read_csv('https://data.un.org/ws/rest/data/IAEG-SDGs,DF_SDG_GLH,1.9/
 df_new = df_csv.pivot(index='TIME_PERIOD', columns='REF_AREA', values='OBS_VALUE')
 df_new.rename(columns={208: 'Denmark', 246: 'Finland', 352: 'Iceland', 578:'Norway',752:'Sweden'},inplace=True)
 df_new.to_csv('data/16_1_4_Share_Safe_After_Dark_Nordics.csv', index=True)
+
+#16.2.1 Share of children from 1 to 14 years of age were subjected to some form of psychological aggression and/or physical punishment at home in the past month World (xxxxx)
+df_csv = pd.read_csv('https://data.un.org/ws/rest/data/IAEG-SDGs,DF_SDG_GLH,1.9/..VC_VAW_PHYPYV.1._T.........../ALL/?detail=full&dimensionAtObservation=TIME_PERIOD&format=csv')
+df_new = df_csv.pivot(index='REF_AREA', columns='TIME_PERIOD', values='OBS_VALUE')
+df_new.rename(index={1:'World'}, inplace=True)
+df_new.to_csv('data/16_2_1_Share_Children_Psychological_Agression_Physical_Punishment_World_Total.csv', index=True)
+
+#16.2.3 Share of women aged 18‑29 years who experienced sexual violence by age 18 World (xxxxx)
+df_csv = pd.read_csv('https://data.un.org/ws/rest/data/IAEG-SDGs,DF_SDG_GLH,1.9/..VC_VAW_SXVLN.1.........../ALL/?detail=full&dimensionAtObservation=TIME_PERIOD&format=csv')
+df_new = df_csv.pivot(index='REF_AREA', columns='TIME_PERIOD', values='OBS_VALUE')
+df_new.rename(index={1:'World'}, inplace=True)
+df_new.to_csv('data/16_2_3_Share_Women_Experience_Sexual_Violence_World_Total.csv', index=True)
+
+#16.2.2 Number of victims of human trafficking Nordics (xxxxx)
+df_csv = pd.read_csv('https://data.un.org/ws/rest/data/IAEG-SDGs,DF_SDG_GLH,1.9/..VC_HTF_DETV.208+246+352+578+752._T.........../ALL/?detail=full&startPeriod=2000-01-01&dimensionAtObservation=TIME_PERIOD&format=csv')
+df_new = df_csv.pivot(index='TIME_PERIOD', columns='REF_AREA', values='OBS_VALUE')
+df_new.rename(columns={208: 'Denmark', 246: 'Finland', 352: 'Iceland', 578:'Norway',752:'Sweden'},inplace=True)
+df_new.to_csv('data/16_2_2_Number_Victims_Human_Trafficing_Nordics.csv', index=True)
+
+#16.2.2 Share of women aged 18‑29 years who experienced sexual violence by age 18 Nordics (xxxxx)
+df_csv = pd.read_csv('https://data.un.org/ws/rest/data/IAEG-SDGs,DF_SDG_GLH,1.9/..VC_VAW_SXVLN.208+246+352+578+752.F.........../ALL/?detail=full&startPeriod=2000-01-01&dimensionAtObservation=TIME_PERIOD&format=csv')
+df_new = df_csv.pivot(index='TIME_PERIOD', columns='REF_AREA', values='OBS_VALUE')
+df_new.rename(columns={208: 'Denmark', 246: 'Finland', 352: 'Iceland', 578:'Norway',752:'Sweden'},inplace=True)
+df_new.to_csv('data/16_2_3_Share_Women_Experience_Sexual_Violence_Nordics.csv', index=True)
+
+#16.2.2 Share of men aged 18‑29 years who experienced sexual violence by age 18 Nordics (xxxxx)
+df_csv = pd.read_csv('https://data.un.org/ws/rest/data/IAEG-SDGs,DF_SDG_GLH,1.9/..VC_VAW_SXVLN.208+246+352+578+752.M.........../ALL/?detail=full&startPeriod=2000-01-01&dimensionAtObservation=TIME_PERIOD&format=csv')
+df_new = df_csv.pivot(index='TIME_PERIOD', columns='REF_AREA', values='OBS_VALUE')
+df_new.rename(columns={208: 'Denmark', 246: 'Finland', 352: 'Iceland', 578:'Norway',752:'Sweden'},inplace=True)
+df_new.to_csv('data/16_2_3_Share_Men_Experience_Sexual_Violence_Nordics.csv', index=True)
