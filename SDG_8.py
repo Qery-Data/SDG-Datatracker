acd069aad893558cf527c48b500de5088f93ee90
aloenne-qery <alf.lonne@qery.no>
2023-07-19 11:28:53 +0200

    fix

--- SDG_8.py
+++ SDG_8.py
@@ -71,7 +71,7 @@ df_new.rename(index={'F':'Female', 'M':'Male'},inplace=True)
 df_new.to_csv('data/8_3_1_Informal_Employment_Female_Male_World_SDG_Regions.csv', index=True)
 
 #8.3.1 Proportion of informal employment Nordics (xxxxx)
-df_csv = pd.read_csv('https://data.un.org/ws/rest/data/IAEG-SDGs,DF_SDG_GLH,1.14/..SL_ISV_IFEM+SL_ISV_IFEM_19ICLS.208+246+352+578+752._T........._T./ALL/?detail=full&startPeriod=2000-01-01&dimensionAtObservation=TIME_PERIOD&format=csv')
+df_csv = pd.read_csv('https://data.un.org/ws/rest/data/IAEG-SDGs,DF_SDG_GLH,1.14/..SL_ISV_IFEM_19ICLS.208+246+352+578+752._T........._T./ALL/?detail=full&startPeriod=2000-01-01&dimensionAtObservation=TIME_PERIOD&format=csv')
 df_new = df_csv.pivot(index='TIME_PERIOD', columns='REF_AREA', values='OBS_VALUE')
 df_new.rename(columns={208: 'Denmark', 246: 'Finland', 352: 'Iceland', 578:'Norway',752:'Sweden'},inplace=True)
 df_new.to_csv('data/8_3_1_Informal_Employment_Nordics.csv', index=True)
